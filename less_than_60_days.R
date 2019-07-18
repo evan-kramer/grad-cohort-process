64bd20f4295678b91a2be93808c4f30d7562d975
evan-kramer <evan.kramer@tn.gov>
2019-07-18 15:23:00 -0500

    Added instructional day counts to less than 60 day code

--- less_than_60_days.R
+++ less_than_60_days.R
@@ -1,6 +1,6 @@
 # Graduation Rate - Automating Less than 60 Days Counts?
 # Evan Kramer
-# 4/11/2019
+# 7/18/2019
 
 # Set up
 options(java.parameters = "-Xmx16G")
@@ -18,31 +18,32 @@ con = dbConnect(
   readRegistry("Environment", "HCU")$EIS_MGR_PWD
 )
 
-# Instructional days, too?  
-#data = 
-as.tbl(dbGetQuery(
-  con,
-  "select isp.isp_id, isp.student_key, type_of_service, first_name, middle_name, last_name, suffix, date_of_birth,
-    isp.school_year, isp.instructional_program_num, enrollment_reason, withdrawal_reason, course_of_study,
-    english_language_background, isp.school_bu_id, begin_date, end_date, primary_district_id, primary_school_id,
-    normal_progression_school, recalc_flg, last_recalc_date, voc_recalc_flg, voc_last_recalc_date, 
-    homeless_residence, homeless_mckinney_vento, homeless_unaccomp_youth, assignment, i.last_instructional_day
-  from instructional_service_period isp
-  join (
-    select isp_id, assignment
-    from instructional_grade
-  ) ig on ig.isp_id = isp.isp_id
-  join (
-    select school_bu_id, school_year, instructional_program_num, max(id_date) as last_instructional_day
-    from scal_id_days
-    group by school_bu_id, school_year, instructional_program_num
-  ) i on (
-    isp.school_bu_id = i.school_bu_id and 
-    isp.school_year = i.school_year and 
-    isp.instructional_program_num = i.instructional_program_num
+# All those eligible and Where they would go back to? 
+reassign = as.tbl(
+  dbGetQuery(
+    con, 
+    str_c(
+      "--select *
+      select student_key, primary_district_id, primary_school_id, count(distinct id_date) as n_days
+      from instructional_service_period
+      inner join instructional_grade 
+      using (student_key, isp_id)
+      inner join scal_id_days
+      using (school_bu_id, instructional_program_num, school_year)
+      where student_key in (
+        select student_key
+        from studentcohortdata
+        left outer join instructional_service_period
+        using (student_key, isp_id) 
+        where cohortyear = extract(year from sysdate) - 4 and 
+          end_date - begin_date < 60
+      ) and school_year >= extract(year from sysdate) - 4 and 
+        assignment in ('09','10','11','12') and
+        type_of_service = 'P' and 
+        id_date between begin_date and end_date
+      group by student_key, primary_district_id, primary_school_id
+      order by student_key, primary_district_id, primary_school_id"
+    )
   )
-  where isp.student_key = 3430677")
 ) %>% 
-  janitor::clean_names() %>%
-  filter(as.integer(assignment) >= 9) %>% 
-  arrange(school_year) 
+  janitor::clean_names()
