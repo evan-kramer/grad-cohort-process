41d84016972a9aff453738b9d6b4f3691011aee2
evan-kramer <evan.kramer@tn.gov>
2019-07-19 12:38:36 -0500

    Added search of enrollment data for students in the wrong cohort, as well as using age data

--- students_in_wrong_cohort.R
+++ students_in_wrong_cohort.R
@@ -18,95 +18,69 @@ con = dbConnect(
   readRegistry("Environment", "HCU")$EIS_MGR_PWD
 )
 
-# Current year
-scd = as.tbl(
-  dbGetQuery(
-    con, str_c(
-      "select scd.student_key, student_ssn, first_name, middle_name, 
-                                last_name, suffix, date_of_birth, gender, student_pin, immigrant, 
-                                date_1st_enrolled_us_school, year_entered_grade9, native_language, 
-                                grad_ell_flag, grad_se_flag, ethnicity, race_i, race_a, race_p, 
-                                race_b, race_w, cohortyear, calc_from, district_no, school_no, 
-                                assignment, eoy_action, withdrawal_reason, completion_type, ell, 
-                                econ_dis, sped, year_withdrawn, included_in_cohort, race_ethnicity, 
-                                manual_intervention, homeless, cte, migrant, scd.isp_id, ell_detail, 
-                                pending_included_in_cohort, completion_period, completion_date, 
-                                allow_upload, t1_t2, original_filename, modified_date, user_id, 
-                                save_as_filename, comments, revised_included_in_cohort, 
-                                reviewer_user_id, reviewed_date, appeal_comments, appeal_date, status 
-                              from studentcohortdata scd 
-                              left outer join studentcohortdocs doc on scd.student_key = doc.student_key 
-                              where scd.cohortyear = ", ifelse(month(today()) >= 10, year(today()) - 1, year(today())) - 4
-    )
+current_cohort = dbGetQuery(
+  con,
+  "select student_key, district_no, school_no, cohortyear, withdrawal_reason, included_in_cohort, assignment revised_included_in_cohort,
+    save_as_filename, assignment
+  from studentcohortdata 
+  left outer join studentcohortdocs
+  using (student_key)
+  where cohortyear = extract(year from sysdate) - 4"
+) %>% 
+  janitor::clean_names() %>% 
+  as.tbl()
+
+# In previous cohorts? 
+dbGetQuery(
+  con,
+  str_c(
+    "select * 
+    from studentcohortdata_historic
+    where student_key in (", current_cohort$student_key, ") and 
+      included_in_cohort = 'Y'"
   )
 ) %>% 
-  janitor::clean_names()
+  janitor::clean_names() %>% 
+  as.tbl()
 
-# Prior years
-scd_prior = tibble()
-for(y in 1:2) {
-  temp = read_csv(str_c("N:/ORP_accountability/data/",
-                        ifelse(month(today()) >= 10, year(today()) - y - 1, year(today()) - y),
-                        "_graduation_rate/student_level.csv")) 
-  if(y == 2) {
-    scd_prior = bind_rows(
-      scd_prior, 
-      transmute(temp, student_key, cohortyear = cohort_year, date_of_birth = as_datetime(dmy(dob)), 
-                withdrawal_reason = withdrawal_code, year_entered_grade9 = year_entered_grade_9,
-                calc_from, included_in_cohort, assignment = as.numeric(grade), completion_type)
-    )
-  } else {
-    scd_prior = bind_rows(
-      scd_prior, 
-      transmute(temp, student_key, cohortyear, date_of_birth, withdrawal_reason, 
-                year_entered_grade9, calc_from, included_in_cohort, assignment, completion_type)
-    )
-  }
-}
+# Wrong grade? 
+filter(current_cohort, assignment != 12 & included_in_cohort %in% c("P", "Y"))
 
-# Changes
-scd_changes = tibble()
-for(y in 1:2) {
-  temp = readxl::read_excel(str_c("N:/ORP_accountability/projects/",
-                                  ifelse(month(today()) >= 10, year(today()) - y - 1, year(today()) - y),
-                                  "_graduation_rate/Documentation for Cohort Changes/Students to be Changed in Cohort.xlsx")) %>% 
-    janitor::clean_names() %>% 
-    transmute(student_key, requested_change, notes) %>% 
-    filter(str_detect(requested_change, "Change COHORTYEAR"))
-  scd_changes = bind_rows(scd_changes, temp)
-}
-rm(y, temp)
+# EIS enrollments in grade 9 in another year? 
+incorrect_enrollments = dbGetQuery(
+  con,
+  str_c(
+    "select student_key, assignment, min(school_year), max(school_year)
+    from instructional_service_period
+    inner join instructional_grade 
+    using (student_key, isp_id)
+    where assignment in ('09', '10', '11', '12') and
+      type_of_service = 'P' and 
+      student_key in (
+        select student_key
+        from studentcohortdata
+        where cohortyear = extract(year from sysdate) - 4 and 
+          included_in_cohort in ('P', 'Y')
+      )
+    group by student_key, school_year, assignment
+    order by student_key, school_year, assignment"
+  )
+) %>% 
+  janitor::clean_names() %>% 
+  as.tbl() %>% 
+  mutate_all("as.double") 
 
-# Predict? 
-c = group_by(scd_changes, student_key) %>% 
-  summarize(requested_change = 1) %>% 
-  ungroup() %>%
-  full_join(scd_prior, by = "student_key") %>% 
-  mutate(age = as_datetime(ymd(str_c(cohortyear, "0701"))) - date_of_birth)
-         # age = format(difftime(as_datetime(ymd(str_c(cohortyear, "0701"))), date_of_birth, units = "days")))
+filter(incorrect_enrollments, min_school_year - assignment != 2006) %>% 
+  distinct(student_key)
 
-# What percent of students in grade 11 were requested to be changed? 
-filter(c, included_in_cohort == "Y") %>% 
-  group_by(cohortyear, assignment, requested_change) %>% 
-  summarize(n = n_distinct(student_key)) %>% 
-  ungroup()
-
-# What percent of students with different YE9 and cohortyear values were requested to be changed? 
-filter(c, included_in_cohort == "Y") %>% 
-  group_by(cohortyear, year_entered_grade9, requested_change) %>% 
-  summarize(n = n_distinct(student_key)) %>% 
-  ungroup() %>% 
-  View()
-
-# Significant differences in ages? 
-t.test(c$age[!is.na(c$age) & c$requested_change == 1],
-       c$age[!is.na(c$age) & is.na(c$requested_change)]
-  
-)
-
-# What percent of change requests had significantly different DOBs? 
-# What percent of change requests were grades other than 12? 
-# What percent of change requests had different cohortyear and ye9 values?
-
-
-
+# By age? 
+dbGetQuery(
+  con,
+  "select student_key, cohortyear, (sysdate - date_of_birth) / 365.25 as age
+  from studentcohortdata
+  where cohortyear = extract(year from sysdate) - 4"
+) %>% 
+  janitor::clean_names() %>% 
+  as.tbl() %>% 
+  filter(!between(age, 12, 22)) %>% # What is reasonable here? 
+  arrange(age)
